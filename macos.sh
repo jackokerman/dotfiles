010782230fd68fa3c3ede2abefbd79f5fdadc1c1
Jack Okerman <jackokerman@hey.com>
2023-02-28 19:33:32 -0800

    Move macos preferences script out of .chezmoiscripts

--- macos.sh
+++ macos.sh
@@ -19,7 +19,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Disable transparency in the menu bar and elsewhere on Yosemite
 defaults write com.apple.universalaccess reduceTransparency -bool true
 
-# Use iMac M1 accent color
+# Use purple iMac M1 accent color
 # https://georgegarside.com/blog/macos/imac-m1-accent-colours-any-mac/
 defaults write -g NSColorSimulateHardwareAccent -bool YES
 defaults write -g NSColorSimulatedHardwareEnclosureNumber -int 7
