8c214417c05b3a44ae533d7c3d02e9d262441828
Jack Okerman <jackokerman@hey.com>
2023-09-21 23:10:09 -0700

    Remove bashdot, use nicknisi install script

--- install.sh
+++ install.sh
@@ -1,17 +1,86 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Check if bashdot is not already available, if not install it
-if ! command -v bashdot &>/dev/null; then
-  echo "bashdot not found, installing..."
-  curl -s https://raw.githubusercontent.com/bashdot/bashdot/master/bashdot >bashdot
-  sudo mv bashdot /usr/local/bin
-  sudo chmod a+x /usr/local/bin/bashdot
-  echo "bashdot installed successfully."
-else
-  echo "bashdot is already installed, skipping."
-fi
+# Taken from curl https://github.com/nicknisi/dotfiles/raw/main/install.sh
 
-# Install dotfiles using bashdot
-pushd ~/dotfiles
-bashdot install default
-popd
+DOTFILES="$(pwd)"
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+error() {
+    echo -e "${COLOR_RED}Error: ${COLOR_NONE}$1"
+    exit 1
+}
+
+warning() {
+    echo -e "${COLOR_YELLOW}Warning: ${COLOR_NONE}$1"
+}
+
+info() {
+    echo -e "${COLOR_BLUE}Info: ${COLOR_NONE}$1"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
+get_linkables() {
+    find -H "$DOTFILES" -maxdepth 3 -name '*.symlink'
+}
+
+setup_symlinks() {
+    title "Creating symlinks"
+
+    for file in $(get_linkables); do
+        target="$HOME/.$(basename "$file" '.symlink')"
+        if [ -e "$target" ]; then
+            info "~${target#$HOME} already exists... Skipping."
+        else
+            info "Creating symlink for $file"
+            ln -s "$file" "$target"
+        fi
+    done
+
+    echo -e
+    info "installing to ~/.config"
+    if [ ! -d "$HOME/.config" ]; then
+        info "Creating ~/.config"
+        mkdir -p "$HOME/.config"
+    fi
+
+    config_files=$(find "$DOTFILES/config" -depth 1 2>/dev/null)
+    for config in $config_files; do
+        target="$HOME/.config/$(basename "$config")"
+        if [ -e "$target" ]; then
+            info "~${target#$HOME} already exists... Skipping."
+        else
+            info "Creating symlink for $config"
+            ln -s "$config" "$target"
+        fi
+    done
+}
+
+case "$1" in
+link)
+    setup_symlinks
+    ;;
+all)
+    setup_symlinks
+    ;;
+*)
+    echo -e $"\nUsage: $(basename "$0") {|link|all}\n"
+    exit 1
+    ;;
+esac
+
+echo -e
+success "Done."
