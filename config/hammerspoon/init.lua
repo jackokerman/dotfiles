bf5f01ef54156519841e07868b54054d9a21b2f2
Jack Okerman <jackokerman@stripe.com>
2025-05-21 14:43:07 -0700

    Add SpoonInstall and ReloadConfiguration spoons

--- config/hammerspoon/init.lua
+++ config/hammerspoon/init.lua
@@ -1,2 +1,12 @@
--- Load Hammerspoon configuration
+-- Load custom modules
 require("rich-link-copy")
+
+-- This spoon is used to automatically download and install other spoons so the
+-- SpoonInstall spooin itself is the only spoon that needs to be downloaded and
+-- installed manually. 
+hs.loadSpoon("SpoonInstall")
+
+-- Automatically reload the configuration when it is changed
+spoon.SpoonInstall:andUse("ReloadConfiguration")
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
