58cc72ef9b3b1a9984f0e365291b49980ca58e52
jackokerman <jackokerman@stripe.com>
2025-11-14 17:04:03 -0800

    Set expose-group-by-app setting to true to work better with Aerospace

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -309,7 +309,7 @@ defaults write com.apple.dock expose-animation-duration -float 0.1
 
 # Don’t group windows by application in Mission Control
 # (i.e. use the old Exposé behavior instead)
-defaults write com.apple.dock expose-group-by-app -bool false
+defaults write com.apple.dock expose-group-by-app -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
 defaults write com.apple.dock mru-spaces -bool false
