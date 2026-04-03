b2b895515ed110da1f15631ba193153716d87b22
Jack Okerman <jackokerman@stripe.com>
2026-04-02 21:20:29 -0700

    fix: show idle tmux sessions as done

--- home/.config/tmux/session-status.sh
+++ home/.config/tmux/session-status.sh
@@ -77,8 +77,9 @@ _session_agent_command() {
 _render_session() {
   local name="$1" state="$2"
   case "${state}" in
-    waiting|done|"") output="${output}${sep}#[fg=#e3d18a] ${name}#[fg=default]" ;;
-    *)       output="${output}${sep}#[fg=#82aaff] ${name}#[fg=default]" ;;
+    waiting) output="${output}${sep}#[fg=#e3d18a] ${name}#[fg=default]" ;;
+    working) output="${output}${sep}#[fg=#82aaff] ${name}#[fg=default]" ;;
+    *)       output="${output}${sep}#[fg=#a6da95] ${name}#[fg=default]" ;;
   esac
   sep="  "
 }
@@ -102,13 +103,13 @@ while IFS= read -r session; do
     IFS=$'\t' read -r _agent state < <(_read_state_record "${STATE_DIR}/${safe}")
     if active_agent=$(_session_agent_command "${session}" 2>/dev/null); then
       if [[ "${active_agent}" != "${_agent}" ]]; then
-        state="waiting"
+        state="done"
       fi
     fi
   elif ! _session_has_known_agent_pane "${session}"; then
     continue
   else
-    state="waiting"
+    state="done"
   fi
 
   rendered_local["${session}"]=1
