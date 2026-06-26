8ffcec9b932a6aa2e797572371e3eaa8197c4ed5
Jack Okerman <jackokerman@stripe.com>
2026-06-25 17:55:22 -0700

    chore: simplify sensitive content check

--- scripts/check-sensitive-content.sh
+++ scripts/check-sensitive-content.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 
 cd "$REPO_ROOT"
-exec dotty guard-check --staged --exclude ".githooks/sensitive-content-patterns"
+exec dotty guard-check --staged
